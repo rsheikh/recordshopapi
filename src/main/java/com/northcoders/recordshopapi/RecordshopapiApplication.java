54d8ccef3117c9a1879d7d4eb4d61de407439752
rsheikh <red.sheikh@gmail.com>
2024-09-05 10:33:45 +0100

    initial commit

---
+++ src/main/java/com/northcoders/recordshopapi/RecordshopapiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordshopapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordshopapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/recordshopapi/RecordshopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordshopapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
