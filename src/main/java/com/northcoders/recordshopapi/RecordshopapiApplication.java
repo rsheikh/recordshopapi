0c0f2be6bc446835c81e3ff415933744e92a4d13
rsheikh <red.sheikh@gmail.com>
2024-09-09 18:27:16 +0100

    feat: Add OpenAPI method for swagger ui

--- src/main/java/com/northcoders/recordshopapi/RecordshopapiApplication.java
+++ src/main/java/com/northcoders/recordshopapi/RecordshopapiApplication.java
@@ -1,13 +1,28 @@
 package com.northcoders.recordshopapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecordshopapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RecordshopapiApplication.class, args);
+
+	}
+
+	@Bean
+	public OpenAPI recordApiInfo() {
+		return new OpenAPI()
+				.info(new Info().title("Record Inventory")
+						.description("Need keep an inventory of all your favourite records? Try this API. \uD83D\uDE3A")
+						.version("v1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
